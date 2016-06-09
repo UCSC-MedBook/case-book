ab5ab169e4569deef8eb8392ea5b0e2586b12cda
Teo Fleming <mokolodi1@gmail.com>
2016-06-09 12:58:20 -0700

    visual improvements

--- webapp/client/main.js
+++ webapp/client/main.js
@@ -2,13 +2,16 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
 Template.body.onRendered(function () {
   $(".ui.dropdown").dropdown();
+
+  // make the dropdowns in the menu work on hover
+  $(".ui.menu .ui.dropdown").dropdown({ on: "hover" });
 });
+
 Template.body.events({
-  'click .createCase': function (event, instance) {
-    $('.ui.modal')
-      .modal('show')
-    ;
+  'click .createCase'(event, instance) {
+    $('.ui.modal').modal('show');
   }
 });
