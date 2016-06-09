fced4bb6e3f125599c2cb40291e3db550b867a90
Teo Fleming <mokolodi1@gmail.com>
2016-06-09 16:49:13 -0700

    fix dropdowns on cases page

--- webapp/client/cases.js
+++ webapp/client/cases.js
@@ -1,3 +1,7 @@
+Template.cases.onRendered(function () {
+  $(".ui.dropdown").dropdown();
+});
+
 Template.cases.events({
   "click .go-to-case-page"(event, instance) {
     FlowRouter.go("showCase");
