af85557d3110fbb68fbf80df0591a7e1109c4567
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-06-09 14:44:43 -0700

    add packages and cases publications

--- webapp/client/main.js
+++ webapp/client/main.js
@@ -15,3 +15,42 @@ Template.body.events({
     $('.ui.modal').modal('show');
   }
 });
+Template.mainPage.onCreated(function() {
+  let instance = this;
+
+  instance.subscribe("Cases", ""); // subscribe immidiately
+
+  function resubscribe(searchString) {
+    // currently we load all the Cases :)
+    // instance.subscribe("Cases", searchString);
+  }
+  let debouncedResubscribe = _.debounce(resubscribe);
+
+  instance.searchString = new ReactiveVar("");
+  instance.autorun((computation) => {
+    let searchString = instance.searchString.get();
+
+    // subscribe immidiately the first time and then debounced resubscribes
+    if (computation.firstRun) {
+      resubscribe(searchString);
+    } else {
+      debouncedResubscribe(searchString);
+    }
+  });
+});
+
+Template.mainPage.helpers({
+  getCases() {
+    let searchString = Template.instance().searchString.get();
+
+    return Cases.find({
+      case_ID: { $regex: new RegExp(searchString) }
+    }, { sort: { case_ID: 1 } });
+  },
+});
+
+Template.searchFields.events({
+  "keyup .search-Cases"(event, instance) {
+    instance.searchString.set(event.target.value);
+  },
+});
