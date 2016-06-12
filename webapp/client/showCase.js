363e3f93946bf3c6575fa44bbad52fbd1fe8bdf9
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-06-12 01:12:51 -0700

    add buttons to insights tab.

--- webapp/client/showCase.js
+++ webapp/client/showCase.js
@@ -3,3 +3,16 @@ Template.showCase.onRendered(function () {
   $('.open-in-new-window').popup();
   $('.tabular.menu .item').tab();
 });
+
+Template.showCase.events({
+  "mouseup .textNorm"(event, instance) {
+        var selection;
+
+        if (window.getSelection) {
+          selection = window.getSelection();
+        } else if (document.selection) {
+          selection = document.selection.createRange();
+        }
+        selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + event.pageX + '/' + event.pageY);
+    }
+  });
