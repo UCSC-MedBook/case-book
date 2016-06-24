6b49b35cd89854bd96a3d2e1d58bec173b853c04
Teo Fleming <mokolodi1@gmail.com>
2016-06-23 17:09:54 -0700

    reworked subscribe/publish for cases

--- webapp/client/cases.js
+++ webapp/client/cases.js
@@ -1,42 +1,50 @@
 Template.cases.onCreated(function() {
   let instance = this;
-  console.log("onCreated cases")
-  var query = {};
-  instance.subscribe("searchCase", query);
+
+  instance.casesQuery = new ReactiveVar({});
+
+  instance.autorun(() => {
+    let query = JSON.parse(JSON.stringify(instance.casesQuery.get()));
+    instance.subscribe("searchCase", query);
+  });
 });
 
 Template.cases.onRendered(function () {
   $(".ui.dropdown").dropdown();
 });
 
+Template.cases.helpers({
+  getCases() {
+    let query = Template.instance().casesQuery.get();
+    console.log("query:", query);
+    console.log("query.onStop:", query.onStop);
+    return Cases.find(query);
+  },
+});
+
 Template.cases.events({
   "click .go-to-case-page"(event, instance) {
     var c = instance.$(".caseid .selected");
     console.log('show case',c);
-    console.log('show case',event, instance);
-    //console.log('current target', event.currentTarget );
+
     FlowRouter.go("showCase");
   },
 });
+
 Template.caseSearchFields.events({
   "change .cancer_type"(event, instance) {
-    var val = $('.cancer_type')
-      .dropdown('get value')
-    ;
-    if (val === null) {
-      console.log('subscribe caseSearch all cases');
-      Meteor.subscribe('searchCase', {});
-    }
-    else {
-      var query = {'cancerType': {$in : val}};
-      console.log('subscribe caseSearch drop down', query);
-      Meteor.subscribe('searchCase', query);
-    }
-  }
-});
+    var val = $('.cancer_type').dropdown('get value');
 
-Template.cases.helpers({
-  getCases() {
-    return Cases.find({});
-  },
+    let query = instance.casesQuery.get();
+
+    if (!val) {
+      delete query.cancer_type;
+    } else {
+      query.cancer_type = val;
+    }
+
+    console.log("setting query:", query);
+    // HERE you're going to have to not refer to instance but Template.instance().parent()
+    instance.casesQuery.set(query);
+  }
 });
--- webapp/server/publications.js
+++ webapp/server/publications.js
@@ -3,21 +3,10 @@ Meteor.publish("singleCase", function(cid) {
   var p = Posts.find({caseId:cid});
   return [ c, p ];
 });
+
 Meteor.publish("searchCase", function(query) {
-  //check(gender, String);
-  //check(cancer_type, [String]);
-  //jcheck(age1, Number);
-  //jcheck(age2, Number);
-  console.log('subscription query', query);
-  //Cases.findWithFacets({gender: gender, age: { $gt: age1, $lt: age2 }, cancer_type: {$in: cancer_type}});
-  if (query === undefined || query === null) {
-    console.log('pub all cases');
-    return Cases.find({});
-  }
-  else {
-    var count = Cases.find(query).count();
-    console.log('pub find', query, 'count', count);
-    return Cases.find(query);
-    //return Cases.findWithFacets({cancer_type: cancer_type});
-  }
+  if (!query) query = {};
+
+  console.log("query:", query);
+  return Cases.find(query, { limit: 20 });
 });
