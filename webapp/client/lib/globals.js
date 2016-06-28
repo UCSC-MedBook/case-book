2d13e831d976ddbaa56333a5ed2d14bbfca7c28d
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-06-28 15:55:59 -0700

    global helpers for templates threeOr

---
+++ webapp/client/lib/globals.js
@@ -0,0 +1,19 @@
+Meteor.startup(() => {
+  //AutoForm.setDefaultTemplate("semanticUI");
+});
+
+Template.registerHelper("firstGreater", function (first, second) {
+  return first > second;
+});
+
+Template.registerHelper("concat", function (first, second) {
+  return first + second;
+});
+
+Template.registerHelper("or", function (first, second) {
+  return first || second;
+});
+
+Template.registerHelper("threeOr", function (first, second, third) {
+  return first || second || third;
+});
