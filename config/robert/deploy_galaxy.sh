7720a0ad893ec068b3d1cc1037ca27774b54a7a3
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-11-24 16:09:13 -0800

    fix once and for all

--- config/robert/deploy_galaxy.sh
+++ config/robert/deploy_galaxy.sh
@@ -7,5 +7,7 @@ export REMOTE_DIRECTORY="/app/case-book"
 export REMOTE_LOG_DIR="/var/log/case-book"
 
 cd webapp
-pwd
-DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy https://stage.casebook.cancercommons.org --settings ../config/settings.json
+echo $METEOR_TOKEN > deployment_token.json
+#echo $METEOR_SETTINGS > deployment_settings.json
+echo "Deploying server at ${METEOR_TARGET}"
+DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com ; METEOR_SESSION_FILE=deployment_token.json meteor deploy ${METEOR_TARGET} --settings ../config/settings.json 
